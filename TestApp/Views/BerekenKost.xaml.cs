25af496c8a0bcae4df589de45d03bb560600b670
Freek <sartrys.freek@hotmail.com>
2021-04-14 10:58:23 +0200

    Kost kan berekend worden (nog te testen)

---
+++ TestApp/Views/BerekenKost.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Filamentenlijst.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BerekenKost : ContentPage
+    {
+        public BerekenKost()
+        {
+            InitializeComponent();
+        }
+
+        public void BerekenKost_Clicked(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                decimal kostPerRolDecimal = decimal.Parse(KostPerRol.Text.ToString());
+                decimal aantalMeterDecimal = decimal.Parse(AantalMeter.Text.ToString());
+                decimal duurtijdInMinutenDecimal = decimal.Parse(Duurtijd.Text.ToString());
+                Kost.Text = (kostPerRolDecimal / 330) * aantalMeterDecimal + (0.0534m * (duurtijdInMinutenDecimal / 60)).ToString();
+            });
+        }
+    }
+}
+
